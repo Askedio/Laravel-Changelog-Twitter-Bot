91fba200064356b5fcd2bb100724e11fcdfe761e
William <gcphost@gmail.com>
2016-04-27 16:24:32 -0700

    .

--- database/migrations/2016_04_21_145449_authors.php
+++ database/migrations/2016_04_21_145449_authors.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class Author extends Migration
+class Authors extends Migration
 {
     /**
       * Run the migrations.
@@ -12,7 +12,6 @@ class Author extends Migration
       */
      public function up()
      {
-       Schema::drop('authors');
         Schema::create('authors', function (Blueprint $table) {
            $table->increments('id');
            $table->string('name');
